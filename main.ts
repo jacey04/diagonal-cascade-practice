ce55d2da8a53933bf8a316d48181b42109534b20
jacey04 <113055266+jacey04@users.noreply.github.com>
2022-09-21 17:36:56 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,6 +6,13 @@ input.onButtonPressed(Button.A, function () {
             led.plot(y, x)
         }
     }
+    for (let x = 0; x <= 4; x++) {
+        for (let y = 0; y <= x; y++) {
+            led.unplot(x, y)
+            basic.pause(100)
+            led.unplot(y, x)
+        }
+    }
 })
 basic.showIcon(IconNames.Heart)
 basic.clearScreen()
