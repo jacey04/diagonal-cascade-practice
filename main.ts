40c861d455542861f83cf2df2c05a2d3892ac95e
jacey04 <113055266+jacey04@users.noreply.github.com>
2022-09-22 16:35:35 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -15,7 +15,18 @@ input.onButtonPressed(Button.A, function () {
     }
 })
 input.onButtonPressed(Button.B, function () {
-	
+    for (let y = 0; y <= 4; y++) {
+        for (let x = 0; x <= 4; x++) {
+            led.plot(x, y)
+            basic.pause(100)
+        }
+    }
+    for (let y = 0; y <= 4; y++) {
+        for (let x = 0; x <= 4; x++) {
+            led.unplot(x, y)
+            basic.pause(100)
+        }
+    }
 })
 basic.showIcon(IconNames.Heart)
 basic.clearScreen()
